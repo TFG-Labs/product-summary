0cc91a343887f538f8a52e2fc81cde2c7fa85f38
Shaheen Karodia <karodia.shaheen@gmail.com>
2022-01-19 08:35:11 +0200

    TLF-136 Decoupled carousel for VTEX CMS

--- react/ProductSummaryList.tsx
+++ react/ProductSummaryList.tsx
@@ -9,49 +9,9 @@ import { ProductList as ProductListStructuredData } from 'vtex.structured-data'
 import ProductSummaryListWithoutQuery from './ProductSummaryListWithoutQuery'
 import { PreferenceType } from './utils/normalize'
 
-const ORDER_BY_OPTIONS = {
-  RELEVANCE: {
-    name: 'admin/editor.productSummaryList.orderType.relevance',
-    value: '',
-  },
-  TOP_SALE_DESC: {
-    name: 'admin/editor.productSummaryList.orderType.sales',
-    value: 'OrderByTopSaleDESC',
-  },
-  PRICE_DESC: {
-    name: 'admin/editor.productSummaryList.orderType.priceDesc',
-    value: 'OrderByPriceDESC',
-  },
-  PRICE_ASC: {
-    name: 'admin/editor.productSummaryList.orderType.priceAsc',
-    value: 'OrderByPriceASC',
-  },
-  NAME_ASC: {
-    name: 'admin/editor.productSummaryList.orderType.nameAsc',
-    value: 'OrderByNameASC',
-  },
-  NAME_DESC: {
-    name: 'admin/editor.productSummaryList.orderType.nameDesc',
-    value: 'OrderByNameDESC',
-  },
-  RELEASE_DATE_DESC: {
-    name: 'admin/editor.productSummaryList.orderType.releaseDate',
-    value: 'OrderByReleaseDateDESC',
-  },
-  BEST_DISCOUNT_DESC: {
-    name: 'admin/editor.productSummaryList.orderType.discount',
-    value: 'OrderByBestDiscountDESC',
-  },
-}
 
 const parseFilters = ({ id, value }: { id: string; value: string }) =>
   `specificationFilter_${id}:${value}`
-
-function getOrdinationProp(attribute: 'name' | 'value') {
-  return Object.keys(ORDER_BY_OPTIONS).map(
-    (key) => ORDER_BY_OPTIONS[key as keyof typeof ORDER_BY_OPTIONS][attribute]
-  )
-}
 
 export interface ProductClickParams {
   position: number
@@ -74,7 +34,7 @@ interface Props {
    * @default ""
    */
   orderBy?:
-    | ''
+    | ''                     //empty string is Relevance
     | 'OrderByTopSaleDESC'
     | 'OrderByPriceDESC'
     | 'OrderByPriceASC'
@@ -118,13 +78,13 @@ function ProductSummaryList(props: PropsWithChildren<Props>) {
     category = '',
     collection,
     hideUnavailableItems = false,
-    orderBy = ORDER_BY_OPTIONS.RELEVANCE.value,
+    orderBy = '', 
     specificationFilters = [],
     maxItems = 10,
-    skusFilter,
-    installmentCriteria,
+    skusFilter = 'ALL_AVAILABLE',
+    installmentCriteria = 'MAX_WITHOUT_INTEREST',
     children,
-    listName: rawListName,
+    listName: rawListName, 
     ProductSummary,
     actionOnProductClick,
     preferredSKU,
@@ -189,94 +149,6 @@ function ProductSummaryList(props: PropsWithChildren<Props>) {
       {children}
     </ProductSummaryListWithoutQuery>
   )
-}
-
-ProductSummaryList.schema = {
-  title: 'admin/editor.productSummaryList.title',
-  description: 'admin/editor.productSummaryList.description',
-  type: 'object',
-  properties: {
-    category: {
-      title: 'admin/editor.productSummaryList.category.title',
-      description: 'admin/editor.productSummaryList.category.description',
-      type: 'string',
-      isLayout: false,
-    },
-    specificationFilters: {
-      title: 'admin/editor.productSummaryList.specificationFilters.title',
-      type: 'array',
-      items: {
-        title:
-          'admin/editor.productSummaryList.specificationFilters.item.title',
-        type: 'object',
-        properties: {
-          id: {
-            type: 'string',
-            title:
-              'admin/editor.productSummaryList.specificationFilters.item.id.title',
-          },
-          value: {
-            type: 'string',
-            title:
-              'admin/editor.productSummaryList.specificationFilters.item.value.title',
-          },
-        },
-      },
-    },
-    collection: {
-      title: 'admin/editor.productSummaryList.collection.title',
-      type: 'string',
-      isLayout: false,
-    },
-    orderBy: {
-      title: 'admin/editor.productSummaryList.orderBy.title',
-      type: 'string',
-      enum: getOrdinationProp('value'),
-      enumNames: getOrdinationProp('name'),
-      default: ORDER_BY_OPTIONS.RELEVANCE.value,
-      isLayout: false,
-    },
-    hideUnavailableItems: {
-      title: 'admin/editor.productSummaryList.hideUnavailableItems',
-      type: 'boolean',
-      default: false,
-      isLayout: false,
-    },
-    maxItems: {
-      title: 'admin/editor.productSummaryList.maxItems.title',
-      type: 'number',
-      isLayout: false,
-      default: 10,
-    },
-    skusFilter: {
-      title: 'admin/editor.productSummaryList.skusFilter.title',
-      description: 'admin/editor.productSummaryList.skusFilter.description',
-      type: 'string',
-      default: 'ALL_AVAILABLE',
-      enum: ['ALL_AVAILABLE', 'ALL', 'FIRST_AVAILABLE'],
-      enumNames: [
-        'admin/editor.productSummaryList.skusFilter.all-available',
-        'admin/editor.productSummaryList.skusFilter.none',
-        'admin/editor.productSummaryList.skusFilter.first-available',
-      ],
-    },
-    installmentCriteria: {
-      title: 'admin/editor.productSummaryList.installmentCriteria.title',
-      description:
-        'admin/editor.productSummaryList.installmentCriteria.description',
-      type: 'string',
-      default: 'MAX_WITHOUT_INTEREST',
-      enum: ['MAX_WITHOUT_INTEREST', 'MAX_WITH_INTEREST'],
-      enumNames: [
-        'admin/editor.productSummaryList.installmentCriteria.max-without-interest',
-        'admin/editor.productSummaryList.installmentCriteria.max-with-interest',
-      ],
-    },
-    listName: {
-      title: 'admin/editor.productSummaryList.analyticsListName.title',
-      type: 'string',
-    },
-  },
 }
 
 export default ProductSummaryList
